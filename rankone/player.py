00c949eab783439ca099825609e26ad16f83e0ea
Tenzin Choedak <tchoedak@gmail.com>
2020-09-05 00:37:05 -0700

    add some init attributes

--- rankone/player.py
+++ rankone/player.py
@@ -1,46 +1,50 @@
+def avg(items):
+    return sum(items) / len(items)
 
 
 class Player(object):
+    def __init__(self, player_id, elo=None, number_of_games=None):
+        self.player_id = player_id
+        self._elo = elo
+        self._number_of_games = number_of_games
 
-	def __init__(self, player_id):
-		self.player_id = player_id
-
-	@property
-	def elo(self):
-		'''
+    @property
+    def elo(self):
+        '''
 		Returns the player's current Elo from ranked games
 		'''
-		return self._elo
+        return self._elo
 
-	@property
-	def number_of_games(self):
-		'''
+    @property
+    def number_of_games(self):
+        '''
 		Returns the number of ranked games the player has played.
 		'''
-		return 5
+        return self._number_of_games
 
-	def boosted_elo(self, elo_gain):
-		'''
+    def boosted_elo(self, elo_gain):
+        '''
 		Returns the player's Elo adjusted for the elo_gain
 		'''
-		return self.elo + elo_gain
+        return self.elo + elo_gain
 
 
 class Team(object):
-	'''
+    '''
 	Holds variable amount of players that make up a team.
 	'''
-	def __init__(self, *players):
-		self.players = players
 
-	@property
-	def combined_elo(self):
-		return sum([player.elo for player in self.players])
+    def __init__(self, *players):
+        self.players = players
 
-	@property
-	def average_elo(self):
-		return avg([player.elo for player in self.players])
+    @property
+    def combined_elo(self):
+        return sum([player.elo for player in self.players])
 
-	@property
-	def combined_weight(self):
-		return sum([player.number_of_games for player in self.players])
+    @property
+    def average_elo(self):
+        return avg([player.elo for player in self.players])
+
+    @property
+    def combined_weight(self):
+        return sum([player.number_of_games for player in self.players])
