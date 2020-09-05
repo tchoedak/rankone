408869bba8c841667bc1601ccae1e6594f9b5e15
Tenzin Choedak <tchoedak@gmail.com>
2020-09-05 00:36:13 -0700

    add tests and fixtures

---
+++ test/test_algorithms.py
@@ -0,0 +1,18 @@
+from rankone.player import Player, Team
+from rankone.elo import EloSystem
+from rankone.algorithms import elo_gains_v1
+import math
+
+
+def test_elo_gains_v1(balanced_teams, red_weaker):
+	elo = EloSystem(elo_gains_v1)
+
+	red, blue = balanced_teams
+	win_elo, lose_elo = elo.calculate_elo_gains(red, blue)
+	assert math.floor(win_elo) == 18
+	assert math.floor(lose_elo) == -19
+
+	red, blue = red_weaker
+	win_elo, lose_elo = elo.calculate_elo_gains(red, blue)
+	assert math.floor(win_elo) == 31
+	assert math.floor(lose_elo) == -32
