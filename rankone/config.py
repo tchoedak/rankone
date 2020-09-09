716b27f8fadc2fd414997bf05c7bcd945ffa8b35
Tenzin Choedak <tchoedak@gmail.com>
2020-09-08 17:16:30 -0700

    add a bot message decorator to use a bot message prefix

--- rankone/config.py
+++ rankone/config.py
@@ -12,3 +12,7 @@ MONITORED_CHANNELS = [752723109710266468]
 
 # CHANNEL_ID of the channel where any channel level logs will go
 LOG_CHANNEL = 752722021674123285
+
+# The Bot will always prefix every message it sends to a channel with this prefix.
+# This is used to make it easy to distinguish bot messages from other bots or from real users.
+BOT_MESSAGE_PREFIX = ':robot: '
--- rankone/reporter.py
+++ rankone/reporter.py
@@ -1,7 +1,16 @@
 import random
 import db
+from config import BOT_MESSAGE_PREFIX
 
 
+def bot_message(func):
+    def wrapper(*args):
+        return BOT_MESSAGE_PREFIX + func(*args)
+
+    return wrapper
+
+
+@bot_message
 def get_report(players):
     report = ', '.join(
         [f'{player.name}: {random.randint(900, 1700)}' for player in players]
@@ -9,7 +18,9 @@ def get_report(players):
     return report
 
 
+@bot_message
 def get_elo_report(players):
 
     report = ', '.join(f'{player.name}: {player.elo:5.0f}' for player in players)
-    return report
+    header = 'Elo updated! '
+    return header + report
