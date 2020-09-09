6bed070d8fcf40fb796def61360d2ed346b33881
Tenzin Choedak <tchoedak@gmail.com>
2020-09-08 22:05:23 -0700

    remove unused match module

--- rankone/match.py
+++
@@ -1,8 +0,0 @@
-class Match(object):
-    def __init__(self, players, message_id, match_created_at):
-        self.players = players
-        self.match_id = message_id
-        self.match_created_at = match_created_at
-        self.red_players = None
-        self.blue_players = None
-        self.winning_team = Nones
