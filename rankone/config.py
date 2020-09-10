afeb482297c886f42c173dd668a4ea6117f16461
Tenzin Choedak <tchoedak@gmail.com>
2020-09-09 19:07:15 -0700

    expose more configs to environment variables

--- rankone/config.py
+++ rankone/config.py
@@ -3,15 +3,15 @@ import os
 
 GUILD = os.getenv('DISCORD_GUILD')
 TOKEN = os.getenv('DISCORD_TOKEN')
-ADMIN_ROLE = 'Admins'
+ADMIN_ROLE = os.getenv('DISCORD_ADMIN_ROLE')
 
 monitored_reactions = {'🔴': 'Red', '🔵': 'Blue'}
 
 # CHANNEL_ID(s) of channels that the bot will monitor match messages and match reaction events
-MONITORED_CHANNELS = [752723109710266468]
+MONITORED_CHANNELS = [int(channel_id) for channel_id in os.getenv('DISCORD_MONITORED_CHANNELS').split(',')]
 
 # CHANNEL_ID of the channel where any channel level logs will go
-LOG_CHANNEL = 752722021674123285
+LOG_CHANNEL = os.getenv('DISCORD_LOG_CHANNEL')
 
 # The Bot will always prefix every message it sends to a channel with this prefix.
 # This is used to make it easy to distinguish bot messages from other bots or from real users.
