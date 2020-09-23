4cfbf3b7a25efaf65d18a50e5793962e841e4a60
Tenzin Choedak <tchoedak@gmail.com>
2020-09-22 21:46:32 -0700

    add type hints to reporter module

--- rankone/reporter.py
+++ rankone/reporter.py
@@ -1,13 +1,15 @@
+from typing import Callable, Any, List
 import random
 from . import db
+from .player import Player
 from .config import BOT_MESSAGE_PREFIX
 
 
-def as_bot(message):
+def as_bot(message: str) -> str:
     return BOT_MESSAGE_PREFIX + message
 
 
-def bot_message(func):
+def bot_message(func: Callable[[Any], Any]) -> Callable[[Any], Any]:
     def wrapper(*args, **kwargs):
         return as_bot(func(*args, **kwargs))
 
@@ -15,7 +17,7 @@ def bot_message(func):
 
 
 @bot_message
-def get_report(players):
+def get_report(players: List[Player]) -> str:
     report = ', '.join(
         [f'{player.display_name}: {random.randint(900, 1700)}' for player in players]
     )
@@ -23,8 +25,7 @@ def get_report(players):
 
 
 @bot_message
-def get_elo_report(*players, has_updated=False):
-
+def get_elo_report(*players: Player, has_updated: bool = False) -> str:
     report = ', '.join(
         f'{player.display_name} [{player.elo:4.0f}]' for player in players
     )
@@ -35,7 +36,7 @@ def get_elo_report(*players, has_updated=False):
 
 
 @bot_message
-def get_leader_report(players):
+def get_leader_report(players: List[Player]) -> str:
     # assumes players are already sorted in descending
     report = f'Top {len(players)} players: \n'
     for i, player in enumerate(players):
